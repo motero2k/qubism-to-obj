16af0e8e6d30568ac75bce7520d8104221a5cedd
Manuel Otero Barbasán <manolo.ob@hotmail.com>
2024-03-04 10:22:43 +0100

    fix: qubism reader and naming

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ const { readQubismJsonToMultiObject3D } = require("./main/qubismReader");
 const qubismScale = 1/256
 const loadedQubismMultiObj3d = readQubismJsonToMultiObject3D("data",qubismScale);
 
-let obj = loadObj(287)
-    // .setName("ramp")
-    .translate([-0.5,-0.5,-0.5])
-let cube = loadObj("cube");
+// let obj = loadObj(287)
+//     // .setName("ramp")
+//     .translate([-0.5,-0.5,-0.5])
+// let cube = loadObj("cube");
 
 // exportObj([obj,obj.copy().rotate([0,0,90]),obj.copy().rotate([0,0,180])], "output.obj");
-exportObj(obj)
-exportObj(cube)
+exportObj(loadedQubismMultiObj3d)
+
 
--- src/main/qubismReader.js
+++ src/main/qubismReader.js
@@ -9,20 +9,21 @@ function readQubismJsonToMultiObject3D(filename, scale=1/256) {
     try {
         const data = fs.readFileSync(`src/input/${filename}.json`, 'utf8');
         const qubismData = JSON.parse(data);
-        const multiObject3D = new MultiObject3d(filename);
+        const multiObject3D = new MultiObject3d(filename).setName(filename);
 
         qubismData.qubes.forEach(qube => {
             if(!shapes[qube.shape]){
                 console.error(`Shape ${qube.shape} not found in shapes folder using default cube.`);
                 qube.shape = 255;
             }
+            console.log(qube.shape, shapes[qube.shape])
             const object3d = loadObj(shapes[qube.shape])
                 // .getPart(0)//get the first part of the object(we only have one part in the shapes obj files)
                 .scale([qube.size_x*scale,qube.size_y*scale,qube.size_z*scale])
                 .translate([qube.pos_x*scale,qube.pos_y*scale,qube.pos_z*scale]);
-            multiObject3D.subObjects.push(object3d);
+            multiObject3D.subObjects.push(...object3d.subObjects);
         });
-        return multiObject3D.subObjects;
+        return multiObject3D;
     } catch (err) {
         console.error('Error reading Qubism JSON:', err);
     }
